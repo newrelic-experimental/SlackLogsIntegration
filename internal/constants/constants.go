50cee2577326937c6a0e524dbf07616ab62d9efd
RagalahariP <rpotti@newrelic.com>
2023-12-08 16:36:31 +0000

    Modify max log size to 1MB

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-        MaxAllowed    = 1000000  // 1GB
+        MaxAllowed    = 1000  // 1MB
 	SlackUserAPIURL = "https://slack.com/api/users.list"
         SlackTeamInfoAPIURL = "https://slack.com/api/team.info"
         SlackBillingInfoAPIURL = "https://slack.com/api/team.billableInfo"
